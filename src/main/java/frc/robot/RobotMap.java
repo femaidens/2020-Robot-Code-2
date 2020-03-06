355b8a9664c3b960e0573fc4db583352b76554d6
Zoey <sunz2@bxscience.edu>
2020-03-06 16:09:56 -0500

    initial commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/* docs */
+
+public class RobotMap{
+	/* joysticks */
+	public static int driveJoyPort = 0;
+	//public static int joyPort2 = 1;
+	/*gyro*/
+	//public static int gyroPort = 2;
+	/*talons*/
+	
+	public static int rearRightPort = 7;
+	public static int rearLeftPort = 4;
+	public static int frontRightPort = 2;
+	public static int frontLeftPort = 8;
+	public static int middleLeftPort = 9;
+	public static int middleRightPort = 3;
+	public static int gyroPort = 15;
+
+	public static int solChannel1 = 10;
+	public static int solChannel2 = 11;
+}
